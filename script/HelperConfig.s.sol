e73ded32297b003e4d24a2bc38c077634a2c9f6a
Dev568145 <bratvold.eirik@gmail.com>
2024-01-24 21:01:32 +0100

    SubId 0

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -36,25 +36,12 @@ contract HelperConfig is Script {
                 interval: 30,
                 vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
                 gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
-                subscriptionId: 8503,
+                subscriptionId: 0,
                 callbackGasLimit: 500000,
                 link: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
                 deployerKey: vm.envUint("PRIVATE_KEY")
             });
     }
-
-    // function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
-    //     return
-    //         NetworkConfig({
-    //             entranceFee: 0.01 ether,
-    //             interval: 30,
-    //             vrfCoordinator: 0x271682DEB8C4E0901D1a1550aD2e64D568E69909,
-    //             gasLane: 0x8af398995b04c28e9951adb9721ef74c74f93e6a478f39e7e0777be13527e7ef,
-    //             subscriptionId: 8503, // need subscriptionId and fund it
-    //             callbackGasLimit: 500000,
-    //             link: 0x514910771AF9Ca656af840dff83E8264EcF986CA
-    //         });
-    // }
 
     function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
         if (activeNetworkConfig.vrfCoordinator != address(0)) {
